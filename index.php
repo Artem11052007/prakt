0fa4f05e63768dcab5985f98d1b19c2759eb7bdb
Artem11052007 <artemmurzak8@gmail.com>
2026-06-10 21:01:39 +0300

    fix nixpacks

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/Model.php';
+require_once __DIR__ . '/model.php';
 require_once __DIR__ . '/Controller.php';
 $model = new Model();
 $controller = new Controller($model);
