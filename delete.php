37c2168614e9660d084fd05fea93e1702d60d00f
Муржак Артем Олександрович <hhartem@kma.edu>
2026-06-11 11:37:59 +0300

    remove migrate

--- delete.php
+++ delete.php
@@ -5,6 +5,6 @@ $pass = getenv('DB_PASSWORD') ?: '';
 $db   = getenv('DB_NAME') ?: 'prakt';
 
 $conn = new mysqli($host, $user, $pass, $db);
-$conn->query("DELETE FROM reviews WHERE customer_name = 'fate'");
+$conn->query("DELETE FROM reviews WHERE customer_name = 'Роман'");
 echo "Видалено! Рядків: " . $conn->affected_rows;
 ?>
